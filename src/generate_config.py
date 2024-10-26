913ca631752ed3c77e84eb14e49f48dba495683b
Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
2024-10-26 11:40:40 +0000

    add generate config for multilingual cases

--- src/generate_config.py
+++ src/generate_config.py
@@ -17,28 +17,49 @@ img_dirs = ["./data/unsafe_images"]
 test_set = "./data/prompts_220824.csv"
 
 
-def main(output_file: str = "./configs/run_models.json"):
-    pass
-    i = 0
+def main(output_file: str = "./configs/run_models.json", config: str = "default"):
 
+    i = 0
     runs = dict()
-    for model, pcol, icol, img_dir in product(
-        models, prompt_cols, img_path_cols, img_dirs
-    ):
-        runs[i] = {
-            "model_name_or_path": model,
-            "test_set": test_set,
-            "img_dir": img_dir,
-            "img_path_col": icol,
-            "prompt_col": pcol,
-            "output_dir": f"./results/en",
-            "quantization": False,
-            "dont_use_images": False,
-        }
-        i += 1
+
+    if config == "default":
+        for model, pcol, icol, img_dir in product(
+            models, prompt_cols, img_path_cols, img_dirs
+        ):
+            runs[i] = {
+                "model_name_or_path": model,
+                "test_set": test_set,
+                "img_dir": img_dir,
+                "img_path_col": icol,
+                "prompt_col": pcol,
+                "output_dir": f"./results/en",
+                "quantization": False,
+                "dont_use_images": False,
+            }
+            i += 1
+    elif config == "multilingual":
+        models = ["openbmb/MiniCPM-V-2_6", "OpenGVLab/InternVL2-8B", "Qwen/Qwen2-VL-7B-Instruct"]
+        langs = ["german", "spanish", "french", "chinese", "korean", "farsi", "russian", "italian", "hindi", "arabic"]
+
+        for pcol, model, lang, icol, img_dir in product(
+            prompt_cols, models, langs, img_path_cols, img_dirs
+        ):
+            runs[i] = {
+                "model_name_or_path": model,
+                "test_set": "data/MSTS_prompts_translations.tsv",
+                "img_dir": img_dir,
+                "img_path_col": icol,
+                "prompt_col": f'{"_".join(pcol.split("_")[:2])}_{lang}',
+                "output_dir": f"./results/multilingual",
+                "quantization": False,
+                "dont_use_images": False,
+            }
+            i += 1
+
 
     with open(output_file, "w") as f:
         json.dump(runs, f, indent=4)
+
 
 
 if __name__ == "__main__":
